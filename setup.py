84d6c17fcf58f6bb4ddc4c025fcb262089556bf7
weirdwizardthomas <tuomas.koristka@gmail.com>
2022-08-13 21:34:16 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='HaikuValidator',
+      version='1.0',
+      description='Simple haiku detector and validator.',
+      author='weirdwizardthomas',
+      author_email='tuomas.koristka@gmail.com',
+      packages=['haiku_validator'],
+      )
